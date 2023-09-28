ea412f98620b54e57217a6c3e382da82b045056b
Likhitha <kandimalla.likhitha@capgemini.com>
2023-09-28 14:40:39 +0530

    files added

---
+++ README.md
@@ -0,0 +1,1 @@
+This Terraform module deploys a Virtual Network in Azure with a subnet or a set of subnets passed in as input parameters.
---
+++ provider.tf
@@ -0,0 +1,5 @@
+module "network" {
+  source  = "Azure/network/azurerm"
+  version = "5.3.0"
+  # insert the 2 required variables here
+}
